515b9f4286d44e4558702b3b4aea9a2a5c4b01bf
권용수 <yskwon0619@gmail.com>
2021-02-06 23:46:58 +0900

    [0206] make cursor animation

---
+++ 0205/cursor.js
@@ -0,0 +1,23 @@
+let mouseCursor = document.querySelector('.cursor');
+let navLinks = document.querySelectorAll(".nav-links li");
+
+window.addEventListener('mousemove',cursorHandler);
+
+function cursorHandler(e){
+  console.log(e);
+  mouseCursor.style.top = e.pageY + 'px';
+  mouseCursor.style.left = e.pageX + 'px';
+  
+}
+
+
+navLinks.forEach(link =>{
+  link.addEventListener('mouseover',()=>{
+    mouseCursor.classList.add('link-grow');
+    link.classList.add('hovered-link');
+  })
+  link.addEventListener('mouseleave',()=>{
+    mouseCursor.classList.remove('link-grow');
+    link.classList.remove('hovered-link');
+  })
+})
