49871ecf6dffb6975bbbabedacf101f552586cde
권용수 <yskwon0619@gmail.com>
2021-03-13 00:02:56 +0900

    [0313] Create slider component

--- 0310/ui_0310_main_group_recommend.js
+++ 0310/ui_0310_main_group_recommend.js
@@ -11,3 +11,25 @@ function menuClickHandler(e) {
   hamburgerIcon.classList.toggle('open');
 }
 menu.addEventListener('click',menuClickHandler);
+
+
+const recommendBtn = document.querySelector('.group__recommend');
+
+const groupFeedBtn = document.querySelector('.group__post');
+
+recommendBtn.addEventListener('click',function(){
+  const groupFeed = document.querySelector('.group__feed');
+  const groupFeed2 = document.querySelector('.group__feed2');
+  groupFeed.classList.remove('group__feed__move');
+  groupFeed2.classList.remove('group__feed2__move')
+  console.log('recommendBtn');
+})
+
+
+groupFeedBtn.addEventListener('click',function(){
+  console.log('groupFeedBtn');
+  const groupFeed = document.querySelector('.group__feed');
+    const groupFeed2 = document.querySelector('.group__feed2');
+  groupFeed.classList.add('group__feed__move');
+   groupFeed2.classList.add('group__feed2__move')
+})
